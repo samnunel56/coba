ac27f8ed5b54291b3e657d75f8e5dd4f797019d3
Samuel <67509713+samnunel56@users.noreply.github.com>
2020-07-16 15:21:24 +0700

    Update coba.java

--- coba.java
+++ coba.java
@@ -1,15 +1,24 @@
-public class Tilang {
-    public static void main(String[] args) {//19
-        boolean SIM = false;//20
-        boolean STNK = true;//21
-//5
-        // cek apakah dia akan ditilang atau tidak//6
-        if(SIM == true){//22
-            if( STNK == true ) {//23
-                System.out.println("Tidak ditilang!");//24
-            }
-        } else {
-            System.out.println("Anda ditilang!");//25
+package collections.shoppingcart; 
+import java.util.List; 
+import java.util.Scanner; 
+    class Hello { 
+        public static void main(String[] args){ 
         }
     }
-}
+    public class cat{ 
+        String breed; 
+        int age; 
+        String color; 
+        void miaow(){ 
+        }
+    }
+    public class LampuLalulintas {
+        public static void main(String[] args) {
+        }
+    }
+    class Person {
+        public String name;
+        public changeName(String newName){
+            this.name = newName;
+        }
+    }
